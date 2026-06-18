10a7430f09000a9501b975714831c2d8bc0ea981
Kauan <kauankrausmaffei@gmail.com>
2026-06-18 16:18:42 -0300

    feat: implement account creation functionality in operation

--- index.js
+++ index.js
@@ -22,6 +22,16 @@ function operation() {
         ],
     },
 ])
-    .then()
+    .then((answer) => {
+        const action = answer['action'] 
+    
+        if (action === 'Criar conta') 
+        createAccount()})
     .catch(err => console.log(err))
+
+    // create an account
+
+    function createAccount() {
+        console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+        console.log(chalk.green('Defina as opções da sua conta a seguir:'))}
 }
