9a6e009b6b134b941bff7c00f99f2532738c44db
Kauan <kauankrausmaffei@gmail.com>
2026-06-19 14:53:47 -0300

    fix: correct syntax and structure in account creation flow

--- index.js
+++ index.js
@@ -25,13 +25,49 @@ function operation() {
     .then((answer) => {
         const action = answer['action'] 
     
-        if (action === 'Criar conta') 
-        createAccount()})
+        if (action === 'Criar conta') {
+            createAccount()
+        }
+    })
     .catch(err => console.log(err))
-
+}
     // create an account
 
     function createAccount() {
         console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
-        console.log(chalk.green('Defina as opções da sua conta a seguir:'))}
+        console.log(chalk.green('Defina as opções da sua conta a seguir:'))
+
+        buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([{
+        name: 'accountName',
+        message: 'Digite um nome para a sua conta:',
+    },
+
+    ])
+    .then((answer) => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
+            buildAccount()
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', function (err) {
+            if (err) {
+                console.log(err)
+            }
+        })
+
+        console.log(chalk.green('Parabéns, sua conta foi criada!'))
+        operation()
+    })
+    .catch(err => console.log(err))
 }
